2100c2228b592261d85d4e611eb1c88b62796403
dlansrhks75 <dlansrhks75@naver.com>
2024-03-19 15:35:34 +0900

    1번 푸쉬

---
+++ hello_a/src/main/java/com/example/demo/HelloAApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAApplication.class, args);
+	}
+
+}
